3a5b9c31f1478de9609cb2de391e7bfd35f76967
kaybow <k.bowmaster@hotmail.com>
2016-04-01 17:17:00 -0400

    Added class to circle

--- js/main.js
+++ js/main.js
@@ -2,6 +2,14 @@ $('.btn-show-hide').on('click', function () {
   $('.box').toggleClass('show-box');
 });
 
-$('.btn-show-hide').on('click', function () {
+$('.btn-move').on('click', function () {
   $('.diamond').toggleClass('move-shape');
+});
+
+$('.btn-collapse-expand').on('click', function () {
+  $('.panel').toggleClass('collapse');
+});
+
+$('.btn-bounce').on('click', function () {
+  $('.circle').toggleClass('bounce');
 });
