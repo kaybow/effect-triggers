7e41b4d818bb2015acba8acd9ca4a6a22a703d3f
kaybow <k.bowmaster@hotmail.com>
2016-04-01 17:05:14 -0400

    Added transition to box to change transparency

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$('.btn-show-hide').on('click', function () {
+  $('.box').toggleClass('show-box');
+});
+
+$('.btn-show-hide').on('click', function () {
+  $('.diamond').toggleClass('move-shape');
+});
